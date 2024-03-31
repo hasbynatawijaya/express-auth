e3744dc12182ffa2a410def59d055984468cc90e
hasbynatawijaya <mhasbyn@gmail.com>
2024-03-31 10:16:32 +0700

    initial commit

---
+++ controllers/logoutController.js
@@ -0,0 +1,45 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const handleLogout = async (req, res) => {
+  //delete token on client
+
+  const cookies = req.cookies;
+  const refreshToken = cookies?.jwt;
+
+  if (!refreshToken) return res.sendStatus(204); //No content
+
+  const foundUser = usersDB.users.find(
+    (person) => person.refreshToken === refreshToken
+  );
+
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+    return res.sendStatus(204); //No content
+  }
+
+  const otherUsers = usersDB.users.filter(
+    (user) => user.refreshToken !== foundUser.refreshToken
+  );
+
+  const currentUser = { ...foundUser, refreshToken: "" };
+
+  usersDB.setUsers([...otherUsers, currentUser]);
+
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "model", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
